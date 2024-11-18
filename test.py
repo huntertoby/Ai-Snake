37bb5039bf92f2b441decc6baa123dba10278b24
huntertoby <63185462+huntertoby@users.noreply.github.com>
2024-11-19 03:41:56 +0800

    Update test.py

--- test.py
+++ test.py
@@ -1,14 +1,8 @@
 import os
-import dotenv
 
-dotenv.load_dotenv()
+# 獲取 GITHUB_ENV 文件的路徑
+env_file = os.getenv('GITHUB_ENV')
 
-# 從環境變數中讀取 secret
-print (os.environ)
-
-secret = os.getenv("AI_SNAKE_ACTIONS_SECRETS")
-
-if secret:
-    print(f"The secret is: {secret}")
-else:
-    print("The secret is not available. Please check the configuration.")
+# 寫入新的環境變數
+with open(env_file, "a") as myfile:
+    myfile.write("MY_VAR=MY_VALUE\n")
