369985088de147efe3c44b61be460cb3c3e11460
huntertoby <63185462+huntertoby@users.noreply.github.com>
2024-11-19 03:28:26 +0800

    Update test.py

--- test.py
+++ test.py
@@ -1,7 +1,8 @@
 import os
 
 # 從環境變數中讀取 secret
-secret = os.getenv("AI_SNAKE_ACTIONS_SECRETS")
+
+secret = os.getenv.get("AI_SNAKE_ACTIONS_SECRETS")
 
 if secret:
     print(f"The secret is: {secret}")
