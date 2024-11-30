6760a327385db8df09e34bf38fde32ba6127a611
huntertoby <63185462+huntertoby@users.noreply.github.com>
2024-11-30 21:57:04 +0800

    Delete test.py

--- test.py
+++
@@ -1,9 +0,0 @@
-import os
-
-# 獲取 GITHUB_TOKEN
-GITHUB_TOKEN = os.getenv("GITHUB_TOKEN")
-
-if GITHUB_TOKEN:
-    print("GITHUB_TOKEN is available.")
-else:
-    print("GITHUB_TOKEN is not available. Please check the configuration.")
